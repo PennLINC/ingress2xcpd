e3de4c503bd2026868f9ea9808fbfb08415ecab2
Steven Meisler <smeisler@g.harvard.edu>
2024-08-22 14:29:23 -0400

    start formalizing package

--- tests/test_ingress2qsirecon/test_main.py
+++ tests/test_ingress2qsirecon/test_main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from ingress2qsirecon.main import get_hello
+from ingress2qsirecon.cli.main import get_hello
+from ingress2qsirecon.cli.parser import parse_args
 
 def it_prints_hi_to_the_project_author() -> None:
     expected = 'Hello, Steven Meisler!'
