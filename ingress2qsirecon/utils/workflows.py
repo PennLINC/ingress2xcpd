1b645ffe979a02f661eac95e9c73b4306fa53534
Steven Meisler <smeisler@g.harvard.edu>
2024-09-01 16:28:18 -0400

    struggling getting the ants compose to work

--- ingress2qsirecon/utils/workflows.py
+++ ingress2qsirecon/utils/workflows.py
@@ -81,7 +81,7 @@ def create_single_subject_wf(subject_layout):
     wf = Workflow(name=wf_name)
 
     # Define input node for the single subject workflow
-    input_node = Node(IdentityInterface(fields=['subject_layout']), name='input_node')
+    input_node = Node(IdentityInterface(fields=['subject_layout',"MNI2009cAsym_to_MNINLin6","MNINLin6_to_MNI2009cAsym"]), name='input_node')
     input_node.inputs.subject_layout = subject_layout
 
     # Create node to parse the input dictionary into its individual components
@@ -118,24 +118,26 @@ def create_single_subject_wf(subject_layout):
 
     # Create nodes to conform anatomicals and save to BIDS layout
     # Add the zooms here for them to work
-    if "t1w_brain" in subject_layout.keys():
-        conform_t1w_node = Node(Conform(), name="conform_t1w")
-        wf.connect(
-            (
-                parse_layout_node,
-                conform_t1w_node,
-                [("t1w_brain", "in_file"), ("bids_t1w_brain", "out_file")],
+    # TMP If false because does not work yet
+    if False:
+        if "t1w_brain" in subject_layout.keys():
+            conform_t1w_node = Node(Conform(), name="conform_t1w")
+            wf.connect(
+                (
+                    parse_layout_node,
+                    conform_t1w_node,
+                    [("t1w_brain", "in_file"), ("bids_t1w_brain", "out_file")],
+                )
             )
-        )
-    if "brain_mask" in subject_layout.keys():
-        conform_mask_node = Node(Conform(), name="conform_mask")
-        wf.connect(
-            (
-                parse_layout_node,
-                conform_mask_node,
-                [("brain_mask", "in_file"), ("bids_brain_mask", "out_file")],
-            ),
-        )
+        if "brain_mask" in subject_layout.keys():
+            conform_mask_node = Node(Conform(), name="conform_mask")
+            wf.connect(
+                (
+                    parse_layout_node,
+                    conform_mask_node,
+                    [("brain_mask", "in_file"), ("bids_brain_mask", "out_file")],
+                ),
+            )
 
     # Write out .b and .bmtxt (DIPY standard)
 
@@ -208,28 +210,66 @@ def create_single_subject_wf(subject_layout):
         )
 
     # Now get transform to MNI2009cAsym
+    MNI_template = subject_layout["MNI_template"]
     if (
-        subject_layout["MNI_TEMPLATE"] == "MNI152NLin6Asym"
-    ):  # tpl-MNI152NLin6Asym_from-MNI152NLin2009cAsym_mode-image_xfm.h5
+        MNI_template == "MNI152NLin6Asym"
+    ):
+        # Get the relevant transforms from templateflow
         MNI2009cAsym_to_MNINLin6 = tflow.get('MNI152NLin6Asym', desc=None, suffix='xfm', extension='h5')
+        input_node.inputs.MNI2009cAsym_to_MNINLin6 = MNI2009cAsym_to_MNINLin6
         MNINLin6_to_MNI2009cAsym = tflow.get('MNI152NLin2009cAsym', desc=None, suffix='xfm', extension='h5')
+        input_node.inputs.MNINLin6_to_MNI2009cAsym = MNINLin6_to_MNI2009cAsym
+
+
+        # Define a function to make a list of two warp files for input to ComposeTransforms
+        def combine_warp_files(file1, file2):
+            return [file1, file2]
+
+        # Create a Function node to make a list of warp files for MNI2subject
+        warp_files_list_MNI2subject = Node(Function(
+            input_names=['file1', 'file2'],
+            output_names=['combined_files'],
+            function=combine_warp_files
+        ), name='list_warp_files_MNI2subject')
+
+        # Create a Function node to make a list of warp files for subject2MNI
+        warp_files_list_subject2MNI = Node(Function(
+            input_names=['file1', 'file2'],
+            output_names=['combined_files'],
+            function=combine_warp_files
+        ), name='list_warp_files_subject2MNI')
+
+
+        # Make the compute nodes for combining transforms
         compose_transforms_node_MNI2subject = Node(ComposeTransforms(), name="compose_transforms_MNI2subject")
-        compose_transforms_node_MNI2subject.inputs.transforms = [
-            nii_to_h5_node_subject2MNI.outputs.xfm_h5_out,
-            MNINLin6_to_MNI2009cAsym,
-        ]
         compose_transforms_node_MNI2subject.inputs.output_warp = str(subject_layout["bids_MNI2subject"]).replace(
-            subject_layout["MNI_TEMPLATE"], "NI152NLin2009cAsym"
+            MNI_template, "MNI152NLin2009cAsym"
         )
         compose_transforms_node_subject2MNI = Node(ComposeTransforms(), name="compose_transforms_subject2MNI")
-        compose_transforms_node_subject2MNI.inputs.transforms = [
-            nii_to_h5_node_MNI2subject.outputs.xfm_h5_out,
-            MNINLin6_to_MNI2009cAsym,
-        ]
         compose_transforms_node_subject2MNI.inputs.output_warp = str(subject_layout["bids_subject2MNI"]).replace(
-            subject_layout["MNI_TEMPLATE"], "NI152NLin2009cAsym"
+            MNI_template, "MNI152NLin2009cAsym"
         )
-        
+
+        # Connect the nodes
+        wf.connect([
+            # For MNI2subject
+            (nii_to_h5_node_MNI2subject, warp_files_list_MNI2subject, [("xfm_h5_out", "file1")]),
+            (input_node, warp_files_list_MNI2subject, [("MNI2009cAsym_to_MNINLin6", "file2")]),
+            (warp_files_list_MNI2subject, compose_transforms_node_MNI2subject, [("combined_files", "warp_files")]),
+
+            # For subject2MNI
+            (input_node, warp_files_list_subject2MNI, [("MNINLin6_to_MNI2009cAsym", "file1")]),
+            (nii_to_h5_node_subject2MNI, warp_files_list_subject2MNI, [("xfm_h5_out", "file2")]),
+            (warp_files_list_subject2MNI, compose_transforms_node_subject2MNI, [("combined_files", "warp_files")]),
+        ])
+
+        # Then Subject --> MNI
+        #compose_transforms_node_subject2MNI = Node(ComposeTransforms(), name="compose_transforms_subject2MNI")
+        #compose_transforms_node_subject2MNI.inputs.warp_files = [MNINLin6_to_MNI2009cAsym, subject_layout["bids_subject2MNI"]]
+        #compose_transforms_node_subject2MNI.inputs.output_warp = str(subject_layout["bids_subject2MNI"]).replace(
+        #    MNI_template, "MNI152NLin2009cAsym"
+        #)
+
     return wf
 
 
